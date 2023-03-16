5e8a63863e0ccb2d3981cfaa615e6207199b095f
Manish Pandey <manish.shiva0107@outlook.com>
2023-03-16 13:33:03 +0530

    Add project files.

---
+++ WebApplicationDevOpsTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationDevOpsTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
